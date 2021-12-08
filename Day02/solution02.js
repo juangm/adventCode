688d12cb3d5f1eb47d09b18326fdeafdda4db70d
juangm <juangarciamarinblog@gmail.com>
2021-12-08 21:06:50 +0100

    Solve the 2nd Day

---
+++ Day02/solution02.js
@@ -0,0 +1,29 @@
+function calculateMeasurements() {
+  const lineReader = require('readline').createInterface({
+    input: require('fs').createReadStream('./Day02/submarine.txt'),
+  });
+
+  let horizontal = 0;
+  let depth = 0;
+  let aim = 0;
+
+  lineReader.on('line', function (movement) {
+    const instructions = movement.split(' ');
+    if (instructions[0] === 'forward') {
+      horizontal = horizontal + parseInt(instructions[1]);
+      depth = depth + aim * parseInt(instructions[1]);
+    } else if (instructions[0] === 'down') {
+      aim = aim + parseInt(instructions[1]);
+    } else if (instructions[0] === 'up') {
+      aim = aim - parseInt(instructions[1]);
+    }
+  });
+
+  lineReader.on('close', () => {
+    console.log(`Horizontal -> ${horizontal}`);
+    console.log(`Depth -> ${depth}`);
+    console.log(`Answer -> ${horizontal * depth}`);
+  });
+}
+
+calculateMeasurements();
